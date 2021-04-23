075afe9a990cf123cc0c439c7c0b27aab8e16b03
mertcandursun <221903570@ogr.uludag.edu.tr>
2021-04-23 19:29:43 +0300

    Proje dosyası ekle.

---
+++ random_Sifre_Odev/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace random_Sifre_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şifre Üret";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(173, 109);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(166, 32);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Üretilen Şifre : ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(107, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(128, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(349, 149);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Şifre Üretici v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ random_Sifre_Odev/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_Sifre_Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void buyukHarfuretA()
+        {
+            //Büyük harf üretici ASCII Kod Tablosunda (65 - 90)
+
+            Random rndChar = new Random();
+            int buyukRandom = rndChar.Next(65, 91);
+            char buyuk = Convert.ToChar(buyukRandom);
+            txtSifre.Text = buyuk.ToString();
+        }
+        public void buyukHarfuretB()
+        {
+            //Büyük harf üretici ASCII Kod Tablosunda (65 - 90)
+
+            Random rndChar = new Random();
+            int buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            char buyuk = Convert.ToChar(buyukRandom);
+            txtSifre.Text += buyuk.ToString();
+        }
+        public void buyukHarfuretC()
+        {
+            //Büyük harf üretici ASCII Kod Tablosunda (65 - 90)
+
+            Random rndChar = new Random();
+            int buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            buyukRandom = rndChar.Next(65, 91);
+            char buyuk = Convert.ToChar(buyukRandom);
+            txtSifre.Text += buyuk.ToString();
+        }
+        public void kucukHarfuretA()
+        {
+            //Küçük harf üretici ASCII Kod Tablosunda (97 - 121)
+
+            Random rndChar = new Random();
+            int kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            char kucuk = Convert.ToChar(kucukRandom);
+            txtSifre.Text += kucuk.ToString();
+        }
+        public void kucukHarfuretB()
+        {
+            //Küçük harf üretici ASCII Kod Tablosunda (97 - 121)
+
+            Random rndChar = new Random();
+            int kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            char kucuk = Convert.ToChar(kucukRandom);
+            txtSifre.Text += kucuk.ToString();
+        }
+        public void kucukHarfuretC()
+        {
+            //Küçük harf üretici ASCII Kod Tablosunda (97 - 121)
+
+            Random rndChar = new Random();
+            int kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            kucukRandom = rndChar.Next(97, 122);
+            char kucuk = Convert.ToChar(kucukRandom);
+            txtSifre.Text += kucuk.ToString();
+        }
+        public void rakamUretA()
+        {
+            //Rakam üretici ASCII Kod tablosunda (48 - 56)
+
+            Random rndChar = new Random();
+            int rakamRandom = rndChar.Next(48, 57);
+            char rakam = Convert.ToChar(rakamRandom);
+            txtSifre.Text += rakam.ToString();
+        }
+        public void rakamUretB()
+        {
+            //Rakam üretici ASCII Kod tablosunda (48 - 56)
+
+            Random rndChar = new Random();
+            int rakamRandom = rndChar.Next(48, 57);
+            rakamRandom = rndChar.Next(48, 57);
+            char rakam = Convert.ToChar(rakamRandom);
+            txtSifre.Text += rakam.ToString();
+        }
+        public void rakamUretC()
+        {
+            //Rakam üretici ASCII Kod tablosunda (48 - 56)
+
+            Random rndChar = new Random();
+            int rakamRandom = rndChar.Next(48, 57);
+            rakamRandom = rndChar.Next(48, 57);
+            rakamRandom = rndChar.Next(48, 57);
+            char rakam = Convert.ToChar(rakamRandom);
+            txtSifre.Text += rakam.ToString();
+        }
+        public void ozelKarakteruretA()
+        {
+            //Özel karakter üretici ASCII Kod tablosunda (33 - 47)
+
+            Random rndChar = new Random();
+            int ozelARandom = rndChar.Next(33, 47);
+            char ozelA = Convert.ToChar(ozelARandom);
+            txtSifre.Text += ozelA.ToString();
+        }
+        public void ozelKarakteruretB()
+        {
+            // Özel karakter üretici ASCII Kod tablosunda (58 - 64)
+
+            Random rndChar = new Random();
+            int ozelBRandom = rndChar.Next(58, 64);
+            char ozelB = Convert.ToChar(ozelBRandom);
+            txtSifre.Text += ozelB.ToString();
+        }
+        public void ozelKarakteruretC()
+        {
+            // Özel karakter üretici ASCII Kod tablosunda (59 - 63)
+
+            Random rndChar = new Random();
+            int ozelBRandom = rndChar.Next(59, 63);
+            char ozelB = Convert.ToChar(ozelBRandom);
+            txtSifre.Text += ozelB.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Value = 100;
+            if (progressBar1.Value == 100)
+            {
+                //Büyük harf metodu çağırılıyor 
+                buyukHarfuretA();
+
+                //Küçük harf metodu çağırılıyor
+                kucukHarfuretA();
+
+                //Rakam metodu çağırılıyor
+                rakamUretA();
+
+                //Özel karakter metodu çağırılıyor ilk aşama
+                ozelKarakteruretA();
+
+                //Büyük harf metodu ikinci aşama
+                buyukHarfuretB();
+
+                //Rakam metodu ikinci aşama
+                rakamUretB();
+
+                //Küçük harf metodu ikinci aşama
+                kucukHarfuretB();
+
+                //Özel karakter metodu çağırılıyor ikinci aşama
+                ozelKarakteruretB();
+
+                //Büyük harf metodu üçüncü aşama
+                buyukHarfuretC();
+
+                //Rakam metodu üçüncü aşama
+                rakamUretC();
+
+                //Küçük harf metodu üçüncü aşama
+                kucukHarfuretC();
+
+                //Özel karakter metodu üçüncü aşama
+                ozelKarakteruretC();
+
+                MessageBox.Show("Şifre başarıyla üretildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+                
+                
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
